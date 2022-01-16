6602af2a917916d5a2a0199a0611512da90a6c09
driscovolo <96077164+driscovolo@users.noreply.github.com>
2022-01-16 23:33:00 +0100

    Update ClaseGit.java

--- src/ClaseGit.java
+++ src/ClaseGit.java
@@ -5,5 +5,6 @@ public static void main (String[]args)
 	System.out.println("Hola");
 	System.out.println("gente");
 	System.out.println("editado remoto");
+	System.out.println("Versiónremota");
 }
 }
